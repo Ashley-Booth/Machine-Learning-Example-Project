748568e6526a03b5f2398a69ca18d0330cc6a33e
Ash <23bootha@gmail.com>
2023-01-21 20:49:52 -0500

    Data filter added

--- DataManager.py
+++ DataManager.py
@@ -2,8 +2,15 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 class DataManager:
-    def __init__(self,path):
+    def __init__(self,path,filter=True):
         self.data = pd.read_csv(path)
+        
+        if filter:
+            columns_keep = ["id", "diagnosis", "radius_mean", "texture_mean", "perimeter_mean", "area_mean", "smoothness_mean"]
+            for column in self.data.columns.values:
+                if column not in columns_keep:
+                    self.data = self.data.drop(column,axis="columns")
+            print(self.data.head())
     
     def print_data(self):
         '''
@@ -27,13 +34,24 @@ class DataManager:
         '''
         Displays the dataframe "head"
         '''
+        print(self.data.head())
     def get_columns(self):
         '''
         Lists the database columns and the types that they each store. 
         '''
+        columns = self.data.columns.values
+        for column in columns:
+            Type = type(self.data[column][0])
+            print(f"{column}:   {Type}")
+        
 
 if __name__=="__main__":
     '''
     The following will only run if this is the file being run (it will not run if this file is imported into another)
+    
     '''
     Dm = DataManager("Data/data.csv")
+    #Dm.summary()
+    #Dm.get_columns()
+
+    
